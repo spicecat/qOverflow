7e97afa04592385da31730852dcdab78e9a2c37d
“spicecat” <ateh0701@gmail.com>
2022-07-06 22:27:18 -0500

    upload captcha and favicon

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,6 @@ import Answer from './Answer'
 import Comment from './Comment'
 import Form from './Form'
 import Navbar from './Navbar'
-import Question from './Questino'
+import Question from './Question'
 
 export { Answer, Comment, Form, Navbar, Question }
--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,12 +1,13 @@
 import { Form } from '../controllers'
 import { loginSchema } from '../services/schemas'
+import CAPTCHA from '../assets/captcha.jpg'
 
 export default function Login() {
 
     return (
         <>
             <Form validationSchema={loginSchema} />
-            
+            <img src={CAPTCHA} alt='CAPTCHA' />
         </>
     )
 }
