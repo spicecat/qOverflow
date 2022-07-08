aaa724c384223417c3b9dd24227448ccbdb56720
C4 Patino <c4patino@gmail.com>
2022-07-08 18:09:48 -0500

    Completed endpoints for mailServices

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,7 +3,6 @@ import LoginController from './LoginController';
 import NavbarController from './NavbarController';
 import QuestionController from './QuestionController';
 import RegisterController from './RegisterController';
-import NavbarController from './NavbarController';
 
 export {
     FormController,
--- src/services/mailServices.js
+++ src/services/mailServices.js
@@ -3,29 +3,41 @@ import { API, API_KEY } from '../var';
 
 const mailApi = API + '/mail';
 
-const postMail = async () => {
+const postMail = async (sender, reciever, subject, text) => {
     const URL = `${mailApi}`;
 
     try {
-        const { user } = await superagent.get(URL).set('key', API_KEY);
-        return user;
+        const status = await superagent
+            .post(URL)
+            .send({
+                sender,
+                reciever,
+                subject,
+                text,
+            })
+            .set('Authorization', `bearer ${API_KEY}`)
+            .then((res) => res.body.success);
+
+        return status;
     } catch (err) {
         return err.status;
     }
 };
 
-const getMail = async username => {
+const getMail = async (username, after) => {
     const URL = `${mailApi}/${username}`;
 
     try {
-        const { user } = await superagent.get(URL).set('key', API_KEY);
-        return user;
+        const messages = await superagent
+            .get(URL)
+            .query({ after })
+            .set('Authorization', `bearer ${API_KEY}`)
+            .then((res) => res.body.messages);
+
+        return messages;
     } catch (err) {
         return err.status;
     }
 };
 
-export {
-    getMail,
-    postMail
-};
+export { getMail, postMail };
