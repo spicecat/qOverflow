c21abe29176a093583cf71d4ad4b4f71302f6332
C4 Patino <c4patino@gmail.com>
2022-07-08 11:20:03 -0500

    Isolated login and register function in userServices

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -13,9 +13,6 @@ const UserContext = createContext(initialUserData);
 export default function UserProvider({ children }) {
     const [userData, setUserData] = useState(initialUserData);
 
-    const login = (values) => console.log(values, 'login');
-    const register = (values) => console.log(values, 'register');
-
     return (
         <UserContext.Provider
             value={{ login, register, setUserData, userData }}
