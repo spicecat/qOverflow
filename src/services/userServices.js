cddeb17572b1212df795b0749aab9e4f7b8be6a2
“spicecat” <ateh0701@gmail.com>
2022-07-07 21:36:48 -0500

    userServices template

---
+++ src/services/userServices.js
@@ -0,0 +1,30 @@
+import superagent from 'superagent'
+import Cookies from 'universal-cookie'
+
+import { } from '../var.js'
+
+// const userServerUrl = serverUrl + '/users', userApiUrl = apiUrl + '/users'
+const cookies = new Cookies()
+
+export const register = async ({ }) => {
+    // const URL = userServerUrl
+    try {
+        return
+    } catch (err) { return err.status }
+}
+
+export const login = async () => {
+}
+
+export const logout = () => {
+}
+
+export const getLocalUser = async () => {
+}
+
+export const getUser = async user_id => {
+}
+
+export const getUsers = async (after = '') => {
+
+}
