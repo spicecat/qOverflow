c5c045c606f087a431cf813cd1bb419dd7cbdef8
“spicecat” <ateh0701@gmail.com>
2022-07-08 16:48:53 -0500

    fix

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -49,7 +49,7 @@ export default function SearchBar() {
             <SearchIconWrapper>
                 <SearchIcon />
             </SearchIconWrapper>
-            <SearchBar placeholder='Search…' />
+            {/* <SearchBar placeholder='Search…' /> */} {/*culprit*/}
         </Search>
     );
 }
--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -13,9 +13,15 @@ const UserContext = createContext(initialUserData);
 export default function UserProvider({ children }) {
     const [userData, setUserData] = useState(initialUserData);
 
+    const registerUser = async values => {
+        console.log(values)
+        const res = await register(values)
+        setUserData(res)
+    }
+
     return (
         <UserContext.Provider
-            value={{ login, register, setUserData, userData }}
+            value={{ login, register: registerUser, setUserData, userData }}
         >
             {children}
         </UserContext.Provider>
