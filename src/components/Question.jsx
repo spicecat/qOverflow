72270b1e26566ff2f87e4326f3b23c5e8f7bcef4
“spicecat” <ateh0701@gmail.com>
2022-07-08 19:59:59 -0500

    question upvote downvote icons

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,3 +1,7 @@
+import { IconButton, ListItem, ListItemText, Tooltip, Typography } from "@mui/material";
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+
 export default function Question({
     answers,
     comments,
@@ -13,8 +17,24 @@ export default function Question({
 }) {
     //This for Q and A
     return (
-        <>
-            {text}
-        </>
+        <ListItem>
+            <div>
+                <Tooltip title='upvote' placement='right'>
+                    <IconButton onClick={() => console.log(11)}>
+                        <ArrowDropUpIcon />
+                    </IconButton>
+                </Tooltip>
+                <br />
+                <Tooltip title='downvote' placement='right'>
+                    <IconButton>
+                        <ArrowDropDownIcon />
+                    </IconButton>
+                </Tooltip>
+            </div>
+
+            <ListItemText>
+                {text}
+            </ListItemText>
+        </ListItem>
     )
 }
