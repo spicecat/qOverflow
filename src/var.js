3760b2527083c7c65ab54b50f307ad53006eb67a
C4 Patino <c4patino@gmail.com>
2022-07-09 07:15:26 -0500

    Removed strinify from createEndpoint function

--- src/var.js
+++ src/var.js
@@ -4,19 +4,20 @@ const API = process.env.REACT_APP_API_ROOT;
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 const stringifyValues = (data = {}) =>
-    Object.fromEntries(Object.entries(data).map(([k, v]) => [k, JSON.stringify(v)]))
+    Object.fromEntries(
+        Object.entries(data).map(([k, v]) => [k, JSON.stringify(v)])
+    );
 
-const createEndpoint = path => (op, endpoint, data) => {
+const createEndpoint = (path) => (op, endpoint, data) => {
     try {
-        console.log(op, path, endpoint, data)
         return superagent[op](`${API}${path}${endpoint}`)
-            .query(stringifyValues(data))
+            .query(data)
             .send(data)
             .set('Authorization', `bearer ${API_KEY}`)
-            .then(res => res.body);
+            .then((res) => res.body);
     } catch (err) {
         return err.status;
     }
-}
+};
 
 export { API, API_KEY, createEndpoint };
