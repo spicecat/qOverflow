8f6caa7f1760d1628df0edfd198522bd734d41f1
“spicecat” <ateh0701@gmail.com>
2022-07-09 22:00:24 -0500

    import fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { ContextProvider } from '../contexts'
+import { ContextProvider } from './contexts'
 import {
     Ask,
     Buffet,
