622cce51efb2e602f214e1c5020e880ea2cccdaa
C4 Patino <c4patino@gmail.com>
2022-07-17 13:10:07 -0500

    Added fetchMail util

---
+++ server/utils/fetchMail.js
@@ -0,0 +1,30 @@
+const createRequest = require('./api');
+const config = require('../config.json');
+
+const defaultAcc = { success: true, requests: [] };
+
+async function fetchMail(
+    url,
+    recentTimestamp = 0,
+    acc = defaultAcc,
+    after = ''
+) {
+    const request = await createRequest('get', url, { after });
+
+    if (!request.success) return config.errorGeneric;
+    if (!request.answers.length) return acc;
+
+    const newAcc = {
+        success: true,
+        requests: [...acc.requests, request],
+    };
+
+    const oldest = request.answers[request.answers.length - 1];
+    if (oldest.createdAt < recentTimestamp) {
+        return newAcc;
+    }
+
+    return fetchMail(url, recentTimestamp, newAcc, oldest['answer_id']);
+}
+
+module.exports = fetchMail;
--- server/utils/isExpired.js
+++
@@ -1,5 +0,0 @@
-function isExpired(object, expiry) {
-    return object.createdAt + expiry < Date.now();
-}
-
-module.exports = isExpired;
