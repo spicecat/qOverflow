22507c7507504159b68599fa94c7990c4f648e41
C4 Patino <c4patino@gmail.com>
2022-07-17 13:04:01 -0500

    Rewrote get mail route

--- server/routes/mail/Get.js
+++ server/routes/mail/Get.js
@@ -1,7 +1,7 @@
 const config = require('../../config.json');
 const paginatedFetch = require('../../utils/paginatedFetch');
-const isExpired = require('../../utils/isExpired');
 const Mail = require('../../db/models/Mail');
+const User = require('../../db/models/User');
 
 async function Get(req, res, next) {
     const user = req.user;
@@ -10,24 +10,36 @@ async function Get(req, res, next) {
         createdAt: 'desc',
     });
 
-    if (cachedMail.length && !isExpired(cachedMail[0], config.mailExpires)) {
+    if (
+        cachedMail.length &&
+        user.lastMailFetch + config.mailExpires > Date.now()
+    ) {
         return res.send({ success: true, messages: cachedMail });
     }
 
     const recentTimestamp = cachedMail[0] ? cachedMail[0].createdAt : 0;
     const { success, requests } = await paginatedFetch(
         `/mail/${username}`,
-        'mail',
         recentTimestamp
     );
 
     if (!success) return res.status(500).send(config.errorGeneric);
 
-    requests.map(async ({ messages }) => {
-        await Mail.create(
-            messages.map((message) => ({ ...message, id: mail_id }))
-        );
-    });
+    await requests
+        .reduce(async (acc, req) => {
+            const reformat = req.messages.map((message) => ({
+                ...message,
+                id: message.mail_id,
+            }));
+            return [...reformat, ...acc];
+        }, [])
+        .map(async (message) => {
+            return await Mail.findByIdAndUpdate(message.id, message, {
+                upsert: true,
+            });
+        });
+
+    await User.findByIdAndUpdate(user.id, { lastMailFetch: Date.now() });
 
     const updatedCache = await Mail.find({ reciever: username }).sort({
         createdAt: 'desc',
