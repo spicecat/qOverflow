4bd1f85039edf09771c73026e7a649bb3b08aef9
C4 Patino <c4patino@gmail.com>
2022-07-17 13:09:39 -0500

    Completed logic to delete comments

--- server/routes/question/DeleteAnswerComment.js
+++ server/routes/question/DeleteAnswerComment.js
@@ -1,11 +1,24 @@
+const Comment = require('../../db/models/Comment');
+const config = require('../../config.json');
+
 async function DeleteAnswerComment(req, res, next) {
     const user = req.user;
     const { questionID, answerID, commentID } = req.params;
+
+    const comment = await Comment.findById(commentID);
+
+    if (!comment) return res.status(404).send(config.errorNotFound);
+
+    if (comment.creator !== user.username) {
+        return res.status(403).send(config.errorForbidden);
+    }
 
     const { success } = await createRequest(
         'delete',
         `/questions/${questionID}/answers/${answerID}/comments/${commentID}`
     );
+
+    await Comment.findByIdAndDelete(commentID);
 
     return success ? res.send() : res.status(500).send('Something went wrong.');
 }
--- server/routes/question/DeleteComment.js
+++ server/routes/question/DeleteComment.js
@@ -1,11 +1,22 @@
+const Comment = require('../../db/models/Comment');
+
 async function DeleteComment(req, res, next) {
-    const user = req.user;
     const { questionID, commentID } = req.params;
+
+    const comment = await Comment.findById(commentID);
+
+    if (!comment) return res.status(404).send(config.errorNotFound);
+
+    if (comment.creator !== user.username) {
+        return res.status(403).send(config.errorForbidden);
+    }
 
     const { success } = await createRequest(
         'delete',
         `/questions/${questionID}/comments/${commentID}`
     );
+
+    Comment.findByIdAndDelete(commentID);
 
     return success ? res.send() : res.status(500).send('Something went wrong.');
 }
