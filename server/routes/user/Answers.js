dfa3803a1edb3b4bb4f70a2d411fc658ab1cec27
C4 Patino <c4patino@gmail.com>
2022-07-17 13:03:51 -0500

    Completed route to fetch user answers and questions

--- server/routes/user/Answers.js
+++ server/routes/user/Answers.js
@@ -1,17 +1,53 @@
-const createRequest = require('../../utils/api');
+const Answer = require('../../db/models/Answer');
+const fetchAnswers = require('../../utils/fetchAnswers');
+const config = require('../../config.json');
 
 async function Answers(req, res, next) {
     const user = req.user;
 
-    const { success, answers } = await createRequest(
-        'get',
-        `/users/${user}/answers`,
-        req.query
+    if (user.lastAnswerFetch + config.answerExpires > Date.now()) {
+        const cachedAnswers = await Answer.find({
+            creator: user.username,
+        })
+            .populate({
+                path: 'creator',
+                select: '-salt',
+            })
+            .sort({ createdAt: 'desc' });
+
+        return res.send(cachedAnswers);
+    }
+
+    const { success, requests } = await fetchAnswers(
+        `/users/${username}/answers`
     );
 
-    return success
-        ? res.send(answers)
-        : res.status(500).send('Something went wrong.');
+    if (!success) return res.status(500).send(config.errorGeneric);
+
+    await requests
+        .reduce(async (acc, req) => {
+            const reformat = req.messages.map((message) => ({
+                ...message,
+                id: message.mail_id,
+            }));
+            return [...reformat, ...acc];
+        }, [])
+        .map(async (message) => {
+            return await Mail.findByIdAndUpdate(message.id, message, {
+                upsert: true,
+            });
+        });
+
+    const cachedAnswers = await Answer.find({
+        creator: user.username,
+    })
+        .populate({
+            path: 'creator',
+            select: '-salt',
+        })
+        .sort({ createdAt: 'desc' });
+
+    return res.send(cachedAnswers);
 }
 
 module.exports = Answers;
--- server/routes/user/Questions.js
+++ server/routes/user/Questions.js
@@ -1,17 +1,17 @@
-const createRequest = require('../../utils/api');
+const Question = require('../../db/models/Question');
+const fetchQuestions = require('../../utils/fetchQuestions');
 
 async function Questions(req, res, next) {
     const user = req.user;
 
-    const { success, questions } = await createRequest(
-        'get',
-        `/users/${user}/questions`,
-        req.query
-    );
+    const cachedQuestions = await Question.find({
+        creator: user.username,
+    }).populate({
+        path: 'creator',
+        select: '-salt',
+    });
 
-    return success
-        ? res.send(questions)
-        : res.status(500).send('Something went wrong.');
+    return res.send(cachedQuestions);
 }
 
 module.exports = Questions;
