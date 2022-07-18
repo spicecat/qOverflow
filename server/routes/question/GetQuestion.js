a1ab48dfb2adc4d109ae6f2735984459ca4b40a1
C4 Patino <c4patino@gmail.com>
2022-07-17 20:42:44 -0500

    Implemented getQuestion functionality

--- server/routes/question/GetQuestion.js
+++ server/routes/question/GetQuestion.js
@@ -1,16 +1,29 @@
 const createRequest = require('../../utils/api');
+const fetchComments = require('../../utils/fetchComments');
+const config = require('../../config.json');
+
+const Question = require('../../db/models/Question');
+const Comment = require('../../db/models/Comment');
 
 async function GetQuestion(req, res, next) {
     const { questionID } = req.params;
 
-    const { success, question } = createRequest(
-        'get',
-        `/questions/${questionID}`
-    );
+    var cachedQuestion = await Question.findById(questionID);
 
-    return success
-        ? res.send(question)
-        : res.status(500).send('Something went wrong.');
+    if (!cachedQuestion) {
+        const { success, question } = createRequest(
+            'get',
+            `/questions/${questionID}`
+        );
+
+        if (!success) return res.status(500).send(config.errorGeneric);
+
+        await Question.create({ ...question, id: question.question_id });
+
+        cachedQuestion = await Question.findById(questionID);
+    }
+
+    return res.send({ success: true, question: cachedQuestion });
 }
 
 module.exports = GetQuestion;
