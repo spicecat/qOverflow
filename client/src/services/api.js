a3d9dfc88a3958810c11147138cdb76dec6abb78
C4 Patino <c4patino@gmail.com>
2022-07-20 13:35:15 -0500

    Reworked superagent function to work with new security

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,5 @@
 import superagent from 'superagent';
 import Throttle from 'superagent-throttle';
-import { API, API_KEY } from 'var';
 
 const throttle = new Throttle({
     active: true,
@@ -9,32 +8,9 @@ const throttle = new Throttle({
     concurrent: 2,
 });
 
-const createEndpoint = (path) => async (op, endpoint, data, auth) => {
-    var request = superagent[op](`${API}${path}${endpoint}`)
+const createEndpoint = (path) => (op, endpoint) =>
+    superagent[op](`${process.env.REACT_APP_API_ROOT}${path}${endpoint}`)
         .use(throttle.plugin)
         .set('Content-Type', 'application/json');
-
-    if (auth) {
-        request = request.set('Authorization', `bearer ${API_KEY}`);
-    }
-
-    if (op === 'get' || op === 'delete') {
-        request
-            .query(data)
-            .then(({ body }) => {
-                console.log('api', op, path, endpoint, data, body);
-                return body;
-            })
-            .catch((err) => console.log(666, err));
-    } else {
-        request
-            .send(data)
-            .then(({ body }) => {
-                console.log('api', op, path, endpoint, data, body);
-                return body;
-            })
-            .catch((err) => console.log(666, err));
-    }
-};
 
 export { createEndpoint };
--- client/src/var.js
+++
@@ -1,4 +0,0 @@
-const API = process.env.REACT_APP_API_ROOT;
-const API_KEY = process.env.REACT_APP_API_KEY;
-
-export { API, API_KEY };
