55e19631926e0b2ed1105bc85c3299a74741dab7
C4 Patino <c4patino@gmail.com>
2022-08-02 13:50:30 -0500

    Redid logic

--- client/src/components/QAComponents/Answer.jsx
+++ client/src/components/QAComponents/Answer.jsx
@@ -25,51 +25,48 @@ export default function Answer({
     text,
     upvotes,
 }) {
-    const { userData } = useUser();
+    const {
+        userData: { level, username },
+    } = useUser();
     const getVote = () => getAnswerVote(question_id, answer_id);
     const updateVote = (data) => updateAnswerVote(question_id, answer_id, data);
-    var canComment =
-        userData?.level >= 3 || creator === userData?.username ? true : false;
+    const canComment = level >= 3 || creator === username;
 
     const postComment = (data) =>
         postAnswerComment(question_id, answer_id, data);
 
-    if (userData.username)
-        return (
-            <span key={answer_id}>
-                <ListItem disablePadding>
-                    <ButtonGroup orientation='vertical'>
-                        <VoteControl
-                            {...{
-                                downvotes,
-                                getVote,
-                                orientation: 'vertical',
-                                updateVote,
-                                upvotes,
-                            }}
-                        />
-                        {accepted && (
-                            <div style={{ textAlign: 'center' }}>
-                                <Tooltip
-                                    title='Accepted Answer'
-                                    placement='right'
-                                >
-                                    <CheckIcon color='success' />
-                                </Tooltip>
-                            </div>
-                        )}
-                    </ButtonGroup>
-                    <ListItemText>
-                        <CreationInfoTag
-                            {...{ createdAt, creator, text: 'answered' }}
-                        />
-                        <ReactMarkdown>{text}</ReactMarkdown>
-                        <CommentControl {...{ canComment, postComment }} />
-                    </ListItemText>
-                </ListItem>
-                <ListItem sx={{ pl: 8 }}>
-                    <AnswerCommentsList {...{ answer_id, comments }} />
-                </ListItem>
-            </span>
-        );
+    return (
+        <span key={answer_id}>
+            <ListItem disablePadding>
+                <ButtonGroup orientation='vertical'>
+                    <VoteControl
+                        {...{
+                            downvotes,
+                            getVote,
+                            orientation: 'vertical',
+                            updateVote,
+                            upvotes,
+                        }}
+                    />
+                    {accepted && (
+                        <div style={{ textAlign: 'center' }}>
+                            <Tooltip title='Accepted Answer' placement='right'>
+                                <CheckIcon color='success' />
+                            </Tooltip>
+                        </div>
+                    )}
+                </ButtonGroup>
+                <ListItemText>
+                    <CreationInfoTag
+                        {...{ createdAt, creator, text: 'answered' }}
+                    />
+                    <ReactMarkdown>{text}</ReactMarkdown>
+                    <CommentControl {...{ canComment, postComment }} />
+                </ListItemText>
+            </ListItem>
+            <ListItem sx={{ pl: 8 }}>
+                <AnswerCommentsList {...{ answer_id, comments }} />
+            </ListItem>
+        </span>
+    );
 }
