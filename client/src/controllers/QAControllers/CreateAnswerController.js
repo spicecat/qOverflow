f87207aa9e592f371814ff15904172ac387ad79e
“spicecat” <ateh0701@gmail.com>
2022-08-02 15:13:36 -0500

    fix createAnswer control

--- client/src/controllers/QAControllers/CreateAnswerController.js
+++ client/src/controllers/QAControllers/CreateAnswerController.js
@@ -1,8 +1,11 @@
+import { useQuestion, useUser } from 'contexts';
 import { CreateAnswer } from 'components/QAComponents';
 
 export default function CommentControlController() {
+    const { questionData: { status } } = useQuestion();
+    const { userData: { level } } = useUser();
 
     return CreateAnswer({
-        
+        canAnswer: level >= 2 && status === 'open'
     });
 }
