9b143d127be5f29a4bc251ac13b69deaa9d9e7b8
C4 Patino <c4patino@gmail.com>
2022-08-02 20:47:37 -0500

    Fixed createAnswer button

--- client/src/components/QAComponents/CreateAnswer.jsx
+++ client/src/components/QAComponents/CreateAnswer.jsx
@@ -1,20 +1,21 @@
-import { Button, Card, CardContent, Tooltip } from '@mui/material'
+import { Button, Card, CardContent, Tooltip } from '@mui/material';
 
-import { AnswerForm } from 'controllers/FormControllers'
-import MdPreview from 'components/MdPreview'
+import { AnswerForm } from 'controllers/FormControllers';
+import MdPreview from 'components/MdPreview';
 
-export default function CreateAnswer({
-    canAnswer,
-    show,
-    toggleShow,
-}) {
-
+export default function CreateAnswer({ canAnswer, show, toggleShow }) {
     return (
         <div>
-            <Tooltip title={!canAnswer ? '' : 'You need to be authenticated to answer or level 5 if the question is protected'}>
+            <Tooltip
+                title={
+                    canAnswer
+                        ? ''
+                        : 'You need to be authenticated to answer or level 5 if the question is protected'
+                }
+            >
                 <span>
                     <Button
-                        disabled={canAnswer}
+                        disabled={!canAnswer}
                         variant='contained'
                         onClick={toggleShow}
                     >
