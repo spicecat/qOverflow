7a220b6c2a23b3c3df5d0474bc2826506a60caa0
C4 Patino <c4patino@gmail.com>
2022-08-10 22:05:21 -0500

    Reworked progress bar

--- client/src/components/PaginatedList.jsx
+++ client/src/components/PaginatedList.jsx
@@ -1,20 +1,11 @@
 import { List, Pagination, Box } from '@mui/material';
 
-import { NoData } from 'components';
-
-export default function PaginatedList({
-    count,
-    Component,
-    data,
-    handleChangePage,
-    noData = true,
-    page,
-}) {
+export default function PaginatedList({ count, Component, data, handleChangePage, page }) {
     return (
         <List sx={{ pl: 2, pr: 2, width: '100%', height: '100%' }}>
-            {data.length
-                ? data.map((item, index) => <Component {...item} key={index} />)
-                : noData && <NoData />}
+            {data.map((item, index) => (
+                <Component {...item} key={index} />
+            ))}
             {count > 1 && (
                 <Box fullWidth display='flex' justifyContent='center' sx={{ margin: '1vh 0' }}>
                     <Pagination
