08b5aee7af62df00ea41aea6fa6e60c57c847132
C4 Patino <c4patino@gmail.com>
2022-08-18 10:40:55 -0400

    Send route returns 404

--- server/routes/mail/Send.js
+++ server/routes/mail/Send.js
@@ -1,4 +1,5 @@
 const Mail = require('server/db/models/Mail');
+const User = require('../../db/models/User');
 const config = require('server/config.json');
 const createRequest = require('server/utils/api');
 
@@ -9,6 +10,12 @@ async function Send(req, res) {
     // Verify required form data exists
     if (!receiver | !subject | !text) {
         return res.status(400).send(config.errorIncomplete);
+    }
+
+    const cachedReciever = await User.findOne({ username: reciever });
+
+    if (!cachedReciever) {
+        return res.status(404).send(config.errorNotFound);
     }
 
     const { success, message } = await createRequest('post', `/mail`, {
