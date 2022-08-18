0538d5b6f605e0766713ea41beeb2c05689b7354
“spicecat” <ateh0701@gmail.com>
2022-08-18 16:06:27 -0400

    warning fix

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -8,13 +8,11 @@ export default function Profile({ userData: { badgeCount, email, level, points }
             <IconButton component={Link} to='/dashboard'>
                 <Gravatar email={email} size={20} style={{ borderRadius: '100%' }} />
             </IconButton>
-            <Typography>
-                <b>{level}</b>
-                <Typography display='inline' m={1}>{points}</Typography>
-                <Typography color='#ffd700' display='inline'>{badgeCount?.gold}</Typography>
-                /<Typography color='#c0c0c0' display='inline'>{badgeCount?.silver}</Typography>
-                /<Typography color='#cd7f32' display='inline'>{badgeCount?.bronze}</Typography>
-            </Typography>
+            <Typography display='inline'><b>{level}</b></Typography>
+            <Typography display='inline' m={1}>{points}</Typography>
+            <Typography color='#ffd700' display='inline'>{badgeCount?.gold}</Typography>
+            /<Typography color='#c0c0c0' display='inline'>{badgeCount?.silver}</Typography>
+            /<Typography color='#cd7f32' display='inline'>{badgeCount?.bronze}</Typography>
         </>
     );
 }
