5f389f52948ecce99c42aebb6a0444a88cf26874
C4 Patino <c4patino@gmail.com>
2022-08-18 14:35:05 -0400

    Completed badges

--- server/db/models/Badge.js
+++ server/db/models/Badge.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Badge = mongoose.Schema({
     title: { type: String, required: true },
     text: { type: String, required: true },
-    rank: { type: String, required: true },
+    rank: { type: String, required: true, enum: ['Gold', 'Silver', 'Bronze'] },
 });
 
 module.exports = mongoose.model('Badge', Badge);
