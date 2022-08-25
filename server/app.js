ae862801d7c35ad266f84dd72f996c0c8dba0758
spicecat <ateh0701@gmail.com>
2022-08-25 13:16:34 -0500

    aws setup wip

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const cors = require('cors');
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
@@ -20,15 +20,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/public', express.static(path.join(__dirname, 'public')));
+// app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.use('/api/users', userRouter);
 app.use('/api/mail', mailRouter);
 app.use('/api/questions', questionRouter);
 
-app.use(express.static(path.join(__dirname, 'public/build')));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/build/index.html'));
-});
+// app.use(express.static(path.join(__dirname, 'public/build')));
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/build/index.html'));
+// });
 
 module.exports = app;
---
+++ server/local.js
@@ -0,0 +1,4 @@
+const app = require('./app'),
+    port = 3001;
+
+app.listen(port, console.log(`Listening on port: ${port}`));
